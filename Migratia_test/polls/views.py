362bd0d15ce668d8787ae9a679c97594a4d9af6d
Mamduh Halawa <mamduhkod@gmail.com>
2024-05-11 14:10:40 +0200

    Created the first views

--- Migratia_test/polls/views.py
+++ Migratia_test/polls/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
-
 def index(request):
-    return HttpResponse('Hello World. You are at the polls index.')
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list}
+    return render(request,"polls/index.html", context)
+
+def detail(request, question_id):
+    return HttpResponse('Youre looking at a question %s.' % question_id)
+
+def results(request, question_id):
+    response = 'Youre looking at the results of question %s'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('Youre voting on question %s.' % question_id)
+
+
