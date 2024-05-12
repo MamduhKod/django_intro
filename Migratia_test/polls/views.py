5b6d6516fcc65889077f5c197fe1f178f74b657f
Mamduh Halawa <mamduhkod@gmail.com>
2024-05-12 18:16:43 +0200

    added more html templates

--- Migratia_test/polls/views.py
+++ Migratia_test/polls/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, render, get_object_or_404
 from django.template import loader
+from django.http import Http404
 
 from .models import Question
 
@@ -11,7 +12,8 @@ def index(request):
     return render(request,"polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse('Youre looking at a question %s.' % question_id)
+    question = get_object_or_404(Question, pk =question_id)
+    return render(request,'polls/detail.html',{"question": question})
 
 def results(request, question_id):
     response = 'Youre looking at the results of question %s'
